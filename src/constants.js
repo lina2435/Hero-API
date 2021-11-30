0641d657603190125354d67b39f67b0655720244
Hayline Solano More <hsolano.more@gmail.com>
2021-11-30 15:55:33 -0500

    refactor

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const AUTH_API = "http://challenge-react.alkemy.org/";
-export const HEROS_BASE = "/api/1589203874773447/";
-export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
